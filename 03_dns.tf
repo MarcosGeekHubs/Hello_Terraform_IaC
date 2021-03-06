f6d652d1ccf3bd9be038512ae0e9dfca01f886a2
Marcos Palacios <m.palacios@geekshubs.com>
2021-03-06 14:05:13 +0100

    DNS for server

--- 03_dns.tf
+++ 03_dns.tf
@@ -9,3 +9,14 @@ resource "digitalocean_domain" "marcware" {
 output "domain_output" {
   value = digitalocean_domain.marcware
 }
+
+
+resource "digitalocean_domain" "web1_marcware" {
+  name = var.web1
+  ip_address = digitalocean_droplet.web-1.ipv4_address
+}
+
+resource "digitalocean_domain" "web2_marcware" {
+  name = var.web2
+  ip_address = digitalocean_droplet.web-2.ipv4_address
+}
