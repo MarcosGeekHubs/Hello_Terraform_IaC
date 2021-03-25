a5990fb6c8439d243ce6a8d59972cce65adbede0
Marcos Palacios <m.palacios@geekshubs.com>
2021-03-25 19:00:28 +0100

    change terraform

--- 04_droplet.tf
+++ 04_droplet.tf
@@ -6,7 +6,7 @@ resource "digitalocean_droplet" "web-2" {
   name      = "web-2"
   region    = "nyc1"
   size      = "s-1vcpu-1gb"
-  user_data = file("userdata_docker-compose.yaml")
+  user_data = file("userdata2.yaml")
   ssh_keys  = [digitalocean_ssh_key.ssh_amrc.fingerprint]
   monitoring = true
 }
